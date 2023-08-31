85a9d671e4b869e602c029ea295a849d6414a31c
Marcos Vasconcellos de Andrade <dcemvasconcellos@dgp.local>
2023-08-31 14:46:56 -0300

    waiting for final evaluation

--- chatbot.py
+++ chatbot.py
@@ -5,33 +5,70 @@
 import openai
 
 # Chave
-openai.api_key = "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"
+openai.api_key = ""
 
 # Função para gerar texto a partir do modelo de linguagem
-df gera_texto(texto):
 
-# Obtém a resposta do modelo de linguagem
-response = openai.Completion.create(
 
-    # Modelo usado
-    # Outros modelos estão disponíveis em https://platform.openai.com/account/rate-limits
-    engine = "text-davinci-003",
+def gera_texto(texto):
 
-    # Texto inicial da conversa com o chatbot
-    prompt = texto,
+    # Obtém a resposta do modelo de linguagem
+    response = openai.Completion.create(
 
-    # Comprimento da resposta gerada pelo modelo
-    max_tokens = 150,
+        # Modelo usado
+        # Outros modelos: https://platform.openai.com/account/rate-limits
+        engine="text-davinci-003",
 
-    # Quantas conclusões gerar para cada prompt
-    n =  5,
+        # Texto inicial da conversa com o chatbot
+        prompt=texto,
 
-    # O texto retornado não conterá a sequência de parada
-    stop = None,
+        # Comprimento da resposta gerada pelo modelo
+        max_tokens=150,
 
-    # Uma medida da aleatoriedade de um texto gerado pelo modelo. Seu valor está entre 0 e 1.
-    # Valores próximos a 1 significam que a saída é mais aleatória, enquanto valores próximos a 0 significam que a saída é muito identificável
-    temperature = 0.8,
-)
+        # Quantas conclusões gerar para cada prompt
+        n=5,
 
-return response.choices[0].text.strip()
+        # O texto retornado não conterá a sequência de parada
+        stop=None,
+
+        # medida da aleatoriedade de um texto gerado pelo modelo entre 0 e 1.
+        # Valores próximos a 1 significam que a saída é mais aleatória,
+        # enquanto valores próximos a 0 significam que a saída é muito
+        # identificável
+        temperature=0.8,
+    )
+
+    return response.choices[0].text.strip()
+
+# Função principal do programa em Python
+
+
+def main():
+
+    print("\nBem-vindo ao GPT-4 Chatbot do Projeto 3 do Curso Gratuito da DSA")
+    print("www.datascienceacademy.com.br")
+    print("(Digite 'sair' a qualquer momento para encerrar o chat)")
+
+    # Loop
+    while True:
+
+        # Coleta a pergunta digitada pelo usuário.
+        user_message = input("\nVocê: ")
+
+        # Se a mensagem for "sair" finaliza o programa.
+        if user_message.lower() == "sair":
+            break
+
+        # variável Python chamada gpt4_prompt.
+        gpt4_prompt = f"\nUsuário: {user_message}\nChatbot:"
+
+        # Obtém a resposta do modelo executando a função gera_texto().
+        chatbot_response = gera_texto(gpt4_prompt)
+
+        # Imprime a resposta do chatbot.
+        print(f"\nChatbot: {chatbot_response}")
+
+
+# Execução do programa (bloco main) em Python
+if __name__ == "__main__":
+    main()
